343687e5c32575ca99e2ba102f904d8936f9029c
venkatesh07 <39082161+venkatesh07@users.noreply.github.com>
2026-09-01 21:38:20 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        //base case
+        if(t.isEmpty()) return "";
+        
+        HashMap<Character, Integer> countT = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        for(int i=0;i<t.length();i++){
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0)+1);
+        }
+        int have = 0, need = countT.size();
+        int[] res = {-1, -1};
+        int resLen = Integer.MAX_VALUE;
+        int l = 0;
+
+        for(int r=0;r<s.length();r++){
+            char c = s.charAt(r);
+            window.put(c, window.getOrDefault(c, 0)+1);
+            if(countT.containsKey(c) && window.get(c).equals(countT.get(c))){
+                have++;
+            }
+            while(have == need){
+                if(r-l+1 < resLen){
+                    resLen = r-l+1;
+                    res[0] = l;
+                    res[1] = r;
+                }
+                char leftChar = s.charAt(l);
+                window.put(leftChar, window.get(leftChar)-1);
+                if(countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)){
+                     have--;
+                }
+                l++;
+            }
+        }
+        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1]+1);
+    }
+}
