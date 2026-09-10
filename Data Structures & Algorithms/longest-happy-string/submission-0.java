924dde1cad7452078e95fc6d91121f66020e4ffd
venkatesh07 <39082161+venkatesh07@users.noreply.github.com>
2026-09-10 18:14:30 +0530

    Add: longest-happy-string - submission-0

---
+++ Data Structures & Algorithms/longest-happy-string/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String longestDiverseString(int a, int b, int c) {
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((k,l) ->Integer.compare(l[0], k[0]));
+
+        if(a > 0) pq.add(new int[]{a, 'a'});
+        if(b > 0) pq.add(new int[]{b, 'b'});
+        if(c > 0) pq.add(new int[]{c, 'c'});
+        
+        
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()){
+            int[] first = pq.poll();
+            if(sb.length() > 1 && sb.charAt(sb.length()-1) == first[1] &&
+            sb.charAt(sb.length()-2) == first[1]){
+                if(pq.isEmpty()) break;
+                int[] second = pq.poll();
+                sb.append((char)second[1]);
+                second[0]--;
+                if(second[0] > 0)  pq.add(second);
+                pq.add(first);
+            }else{
+                sb.append((char)first[1]);
+                first[0]--;
+                if(first[0] > 0)  pq.add(first);
+            }
+        }
+        return sb.toString();
+    }
+}
